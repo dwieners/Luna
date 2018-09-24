1c09a2d6e5e416d57b85a8cacc79f74bab477a3c
Dominik Wieners <dom.wieners@gmx.net>
2018-09-24 21:54:57 +0200

    Add wallpaper feature

--- app/src/main/java/de/dominikwieners/luna/view/UnsplashDetailActivity.kt
+++ app/src/main/java/de/dominikwieners/luna/view/UnsplashDetailActivity.kt
@@ -1,8 +1,12 @@
 package de.dominikwieners.luna.view
 
+import android.app.WallpaperManager
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModelProviders
 import android.content.Context
 import android.databinding.DataBindingUtil
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
@@ -15,7 +19,11 @@ import de.dominikwieners.luna.di.LunaApplication
 import de.dominikwieners.luna.model.UnsplashPictureResponse
 import javax.inject.Inject
 import android.util.DisplayMetrics
+import android.widget.Toast
+import com.squareup.picasso.Callback
 import de.dominikwieners.luna.viewmodel.UnsplashDetailViewModel
+import java.io.IOException
+import java.lang.Exception
 import java.sql.Timestamp
 import java.text.SimpleDateFormat
 
@@ -29,6 +37,8 @@ class UnsplashDetailActivity : AppCompatActivity() {
     lateinit var unsplashPictureResponse:UnsplashPictureResponse
     lateinit var unsplashDetailViewModel:UnsplashDetailViewModel
 
+    var pictureDrawable = MutableLiveData<Drawable>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         (application as LunaApplication).getComponent().inject(this)
@@ -39,8 +49,10 @@ class UnsplashDetailActivity : AppCompatActivity() {
         initBinding(unsplashDetailViewModel)
         initToolbar()
         setImages()
-
+        setWallpaperImage()
     }
+
+
 
     private fun initViewModel(unsplashPictureResponse: UnsplashPictureResponse){
         unsplashDetailViewModel = ViewModelProviders.of(this).get(UnsplashDetailViewModel::class.java)
@@ -61,8 +73,29 @@ class UnsplashDetailActivity : AppCompatActivity() {
     }
 
     private fun setImages(){
-       Picasso.get().load(binding.viewmodel!!.detail.value!!.urls?.small).into(binding.unsplashDetailIvPhoto)
-       Picasso.get().load(binding.viewmodel!!.detail.value!!.user?.profile_image?.small).into(binding.unsplashDetailIvUser)
+       Picasso.get().load(binding.viewmodel!!.detail.value!!.urls?.small).into(binding.unsplashDetailIvPhoto, object : Callback {
+           override fun onSuccess() {
+               pictureDrawable.value = binding.unsplashDetailIvPhoto.drawable
+           }
+
+           override fun onError(e: Exception?) {
+               TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+           }
+       })
+
+        Picasso.get().load(binding.viewmodel!!.detail.value!!.user?.profile_image?.small).into(binding.unsplashDetailIvUser)
+    }
+
+    private fun setWallpaperImage(){
+        binding.unsplashDetailBuWallpaper.setOnClickListener {
+            var wallpaperManager:WallpaperManager = WallpaperManager.getInstance(this)
+            try {
+                wallpaperManager.setBitmap((pictureDrawable.value as BitmapDrawable).bitmap)
+                Toast.makeText(this,"Wallpaper successfully changed",Toast.LENGTH_LONG).show()
+            }catch (e:IOException){
+                e.printStackTrace()
+            }
+        }
     }
 
     override fun onBackPressed() {
@@ -76,4 +109,6 @@ class UnsplashDetailActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
+
+
 }
